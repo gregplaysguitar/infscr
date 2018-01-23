1a6200037b5ab093686b066ade53bcb879d2cb15
Greg Brown <greg@gregbrown.co.nz>
2018-01-24 10:23:44 +1300

    Purge jquery

--- infscr.js
+++ infscr.js
@@ -1,19 +1,56 @@
+/* Notes
+
+- canOverwrite method to check whether the current page can be overwritten
+  by cached content?
+
+- Need a "sanity check" value which can be compared to see if the cached
+  content matches. Default could be length of html, or don't use by default.
+  For cag it's the href of the next link as this contains the order_token
+
+- use #fragment to remember which page we're at, might solve above problems
+
+- TODO jquery purge - check
+
+*/
+
 (function (window, factory) {
   // universal module definition - thanks @desandro
   /* eslint-disable eqeqeq, no-undef */
   if (typeof define == 'function' && define.amd) {
     // AMD
-    define(['jquery'], factory);
+    define([], factory)
   } else if (typeof module == 'object' && module.exports) {
     // CommonJS
-    module.exports = factory(require('jquery'));
+    module.exports = factory()
   } else {
     // browser global
-    window.InfScr = factory(window.jQuery);
+    window.InfScr = factory()
   }
   /* eslint-enable eqeqeq, no-undef */
-}(window, function factory ($) {
-  'use strict';
+}(window, function factory () {
+  'use strict'
+
+  var sessionCache = {
+    set: function (key, value) {
+      return window.sessionStorage.setItem(key, value)
+    },
+    get: function (key) {
+      return window.sessionStorage.getItem(key)
+    }
+  }
+
+  var dummyCache = {
+    set: function () {},
+    get: function () {}
+  }
+
+  function getOffset (el) {
+    el = el.getBoundingClientRect()
+    return {
+      left: el.left + window.scrollX,
+      top: el.top + window.scrollY
+    }
+  }
 
   function InfScr (container, options) {
     /* Create an infinite-scrolling page from a container with pagination.
@@ -30,140 +67,188 @@
        If bind is false, the resize method must be called before the
        auto-load behaviour will work. */
 
-    options = $.extend({
+    options = Object.assign({
+      cache: sessionCache,
       nextSelector: null,
       contentSelector: null,
       autoOffset: null,
       bind: false,
       get_url: function (link) {
-        return link.attr('href');
+        return link.getAttribute('href')
       },
       get_content: function (data) {
-        return data;
+        return data
       },
       getCacheKey: function () {
         // TODO get a unique key for the instance/url etc
-        return 'infscr_cache' + window.location.pathname + window.location.search;
+        return 'infscr_cache' + window.location.pathname + window.location.search
       },
       beforeLoad: null,
       onLoad: null
-    }, options);
-    var loading = false;
-    var winHeight;
-    var containerOffset;
-    var autoLoadThreshold;
-    var setThreshold;
-    var xhr;
+    }, options)
+    var loading = false
+    var winHeight
+    var containerOffset
+    var autoLoadThreshold
+    var setThreshold
+    // var xhr
+    var nextLink = container.querySelector(options.nextSelector)
 
-    var cached = window.sessionStorage.getItem(options.getCacheKey());
+    if (!options.cache) {
+      options.cache = dummyCache
+    }
+
+    var cached = options.cache.get(options.getCacheKey())
     if (cached) {
       // assume cached is a superset of the current items. TODO check this is
       // actually the case
-      // console.log('Adding', container.html().length, cached.length);
+      // console.log('Adding', container.html().length, cached.length)
 
-      var tempEl = $('<div>').html(cached);
-      var currentItems = container.find(options.contentSelector);
-      var newItems = tempEl.find(options.contentSelector)
-      .slice(currentItems.length);
-      var nextHref = tempEl.find(options.nextSelector).attr('href');
+      var newContainer = document.createElement('div')
+      newContainer.innerHTML = cached
+      var currentItems = container.querySelectorAll(options.contentSelector)
+      var newItems = newContainer.querySelectorAll(options.contentSelector)
+      // newItems = Array.prototype.slice.call(newItems).slice(currentItems.length)
+      var newNextLink = newContainer.querySelector(options.nextSelector)
+      var nextHref = newNextLink ? newNextLink.getAttribute('href') : null
 
-      container.find(options.nextSelector).attr('href', nextHref);
-      container.append(newItems);
-      options.onLoad(newItems, container);
-    } else {
-      window.sessionStorage.setItem(options.getCacheKey(), container.html());
-    }
-
-    function cancelXhr () {
-      if (xhr) {
-        xhr.abort();
-      }
-    }
-
-    function append (url) {
-      if (!loading) {
-        container.addClass('loading-more');
-        loading = true;
-        cancelXhr();
-        if (options.beforeLoad) {
-          options.beforeLoad();
+      var frag = document.createDocumentFragment()
+      for (var i = 0; i < newItems.length; i++) {
+        if (i >= currentItems.length) {
+          frag.appendChild(newItems)
         }
-        // TODO get rid of jquery, use window.fetch
-        xhr = $.get(url, function (data) {
-          var html = options.get_content(data);
-          var content = $('<div>').html(html);
-          var nextHref = content.find(options.nextSelector).attr('href');
-          var link = container.find(options.nextSelector);
-          var newContent = content.find(options.contentSelector);
+      }
 
-          if (link.length) {
-            newContent.insertBefore(link);
-          } else {
-            container.append(newContent);
-          }
-          container.removeClass('loading-more');
+      if (nextLink) {
+        container.insertBefore(frag, nextLink)
+        if (nextHref) {
+          nextLink.setAttribute('href', nextHref)
+        } else {
+          nextLink.remove()
+        }
+      } else {
+        container.appendChild(frag)
+      }
 
-          if (nextHref) {
-            link.attr('href', nextHref);
-          } else {
-            link.remove();
-          }
+      // TODO
+      // options.onLoad(newItems, container)
+      options.onLoad(null, container)
+    } else {
+      options.cache.set(options.getCacheKey(), container.innerHTML)
+    }
 
-          window.sessionStorage.setItem(options.getCacheKey(), container.html());
+    function cancelFetch () {
+      // TODO
+      // if (xhr) {
+      //   xhr.abort()
+      // }
+    }
 
-          if (options.onLoad) {
-            options.onLoad(newContent, container);
+    function append () {
+      var next = container.querySelector(options.nextSelector)
+      if (!loading && next) {
+        container.classList.add('loading-more')
+        loading = true
+        cancelFetch()
+        if (options.beforeLoad) {
+          options.beforeLoad()
+        }
+
+        var params = {
+          method: 'get',
+          headers: {
+            'X-Requested-With': 'XMLHttpRequest'
           }
-          loading = false;
-          if (setThreshold) {
-            // won't exist if not auto-loading
-            setThreshold();
-          }
-        });
+        }
+
+        return window.fetch(options.get_url(next), params)
+          .then(function (response) {
+            return response.text()
+          }).then(function (text) {
+            return options.get_content(text)
+          }).then(function (html) {
+            var content = document.createElement('div')
+            content.innerHTML = html
+            var newNextLink = content.querySelector(options.nextSelector)
+            var newContent = content.querySelectorAll(options.contentSelector)
+
+            var frag = document.createDocumentFragment()
+            for (var i = 0; i < newContent.length; i++) {
+              frag.appendChild(newContent[i])
+            }
+            if (nextLink) {
+              container.insertBefore(frag, nextLink)
+            } else {
+              container.appendChild(frag)
+            }
+
+            container.classList.remove('loading-more')
+
+            if (newNextLink) {
+              nextLink.setAttribute('href', newNextLink.getAttribute('href'))
+            } else {
+              nextLink.remove()
+            }
+
+            options.cache.set(options.getCacheKey(), container.innerHTML)
+
+            if (options.onLoad) {
+              options.onLoad(newContent, container)
+            }
+            loading = false
+            if (setThreshold) {
+              // won't exist if not auto-loading
+              setThreshold()
+            }
+          })
       }
     }
 
-    container.on('click', options.nextSelector, function () {
-      append(options.get_url($(this)));
-      return false;
-    });
+    if (nextLink) {
+      nextLink.addEventListener('click', function (e) {
+        append()
+        e.preventDefault()
+      })
+    }
 
     var obj = {
-      cancel: cancelXhr
-    };
+      cancel: cancelFetch
+    }
 
     function resize (w, h) {
-      winHeight = h;
-      containerOffset = container.offset();
-      setThreshold();
+      winHeight = h
+      containerOffset = getOffset(container)
+      setThreshold()
     }
 
     function scroll (t, l) {
       if (t > autoLoadThreshold) {
-        container.find(options.nextSelector).trigger('click');
+        append()
       }
     }
 
     if (options.autoOffset) {
-      container.find(options.nextSelector).hide();
+      if (nextLink) {
+        nextLink.style.display = 'none'
+      }
 
       setThreshold = function () {
         if (winHeight === undefined) {
-          return;
+          return
         }
-        autoLoadThreshold = containerOffset.top + container.outerHeight() -
-        winHeight - options.autoOffset;
-      };
+        autoLoadThreshold = containerOffset.top + container.offsetHeight -
+        winHeight - options.autoOffset
+      }
 
       // if (options.bind) {
-      //   bind_handlers(resize, scroll);
+      //   bind_handlers(resize, scroll)
       // }
-      obj.resize = resize;
-      obj.setThreshold = setThreshold;
-      obj.scroll = scroll;
+      obj.resize = resize
+      obj.setThreshold = setThreshold
+      obj.scroll = scroll
     }
-    return obj;
+    return obj
   }
 
-  return InfScr;
-}));
+  return InfScr
+}))
